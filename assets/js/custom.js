bb6ebecffd9250b8a53c5360d4eb54822f7f7f9c
Sanjay <sanjay.nediyara@mobme.in>
2016-01-13 12:40:11 +0530

    Initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,34 @@
+﻿
+/*=============================================================
+    Authour URL: www.designbootstrap.com
+
+    http://www.designbootstrap.com/
+
+    License: MIT
+
+    http://opensource.org/licenses/MIT
+
+    100% Free To use For Personal And Commercial Use.
+
+    IN EXCHANGE JUST TELL PEOPLE ABOUT THIS WEBSITE
+   
+========================================================  */
+
+$(document).ready(function () {
+    // SCROLL SCRIPTS 
+        $('.scroll-me a').bind('click', function (event) { //just pass scroll-me class and start scrolling
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutQuad');
+        event.preventDefault();
+        });
+    //COLLAPSE HEADER ON SCRLL
+        $(window).scroll(function () {
+            if ($(".navbar").offset().top > 40) {
+                $(".navbar-fixed-top").addClass("navbar-pad-original");
+            } else {
+                $(".navbar-fixed-top").removeClass("navbar-pad-original");
+            }
+        });
+});
